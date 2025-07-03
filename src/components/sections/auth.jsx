ed6634bdf89b743fabe4482e5ee9d7131719f55f
DrewShirai <andrewdemetillo123@gmail.com>
2025-07-04 03:59:52 +0800

    Added Login Form with design

---
+++ src/components/sections/auth.jsx
@@ -0,0 +1,18 @@
+import logo from '/snapserve_logo.jpg'
+
+function Auth() {  
+    return <div id="login-form">
+        <img id="logo"src={logo}/>
+    <div>
+        <p className='subtitle'>Email address</p>
+        <input className="inputAuth" placeholder="example@gmail.com"></input>
+
+
+        <p className='subtitle'>Password</p>
+        <input className="inputAuth" placeholder ="Password"></input>
+    </div>
+        <button id="login-btn">LOG IN</button>
+    </div>
+}
+
+export default Auth;
