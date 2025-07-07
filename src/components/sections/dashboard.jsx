dd6416e00683bd2a12388d38eff2859a58856a0b
DrewShirai <andrewdemetillo123@gmail.com>
2025-07-07 22:20:01 +0800

    naa sa kunoy e save

--- src/components/sections/dashboard.jsx
+++ src/components/sections/dashboard.jsx
@@ -10,28 +10,37 @@ function Dashboard(){
     <div className="menu">
         <div className="menu-list">
             <div className='navigation'>
-                <img className='menu-icons' src='vite.svg'/>
+                <img className='menu-icon' src='vite.svg'/>
                 <p>Dashboard</p>
             </div>
             <div className='navigation'>
-                <img className='menu-icons' src='vite.svg'/>
+                <img className='menu-icon' src='vite.svg'/>
                 <p>Vendors</p>
             </div>
             <div className='navigation'>
-                <img className='menu-icons' src='vite.svg'/>
+                <img className='menu-icon' src='vite.svg'/>
                 <p>Stores</p>
             </div>
             <div className='navigation'>
-                <img className='menu-icons' src='vite.svg'/>
+                <img className='menu-icon' src='vite.svg'/>
                 <p>Users</p>
             </div>
             <div className='navigation'>
-                <img className='menu-icons' src='vite.svg'/>
+                <img className='menu-icon' src='vite.svg'/>
                 <p>Reports</p>
             </div>
         </div>
         <button id="logout-btn">LOG OUT</button>
     </div> 
+     <div className="dashboard">
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+     </div>
+     <div className='statistics'>
+
+     </div>
     </div>
     
 }
